ca62a0d938e91aa50f3ee1f2ac37a144bb615944
Lautaro Demonte <lauty.d.p@gmail.com>
2022-11-09 18:10:45 -0300

    agrego las carpetas en pug sobre las cuales voy a traducir el handlebars

--- pug/app.js
+++ pug/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bp = require("body-parser");
 const routers = require("./routers");
+const handlebars = require("express-handlebars");
+const Contenedor = require("./controllers/productsController");
+const productos = new Contenedor("./controllers/productos.json");
 const app = express();
 const PORT = 8080;
 
@@ -8,11 +11,36 @@ const PORT = 8080;
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-/* visibilizo la carpeta public */
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname: "hbs",
+        defaultLayout: "index.hbs",
+        layoutsDir: __dirname + "/views",
+    })
+);
 
-app.use("/static", express.static(__dirname + "/public"));
+app.set("views", "./views");
+app.set("view engine", "hbs");
 
 app.use("/", routers);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("formulario", {
+        style: "formulario.css",
+        title: "Formulario Handlebars",
+    });
+});
+
+app.post("/", async (req, res) => {
+    console.log(`post req recibida con exito`);
+    const data = req.body;
+    console.log(data);
+    const nuevoProducto = await productos.save(data);
+    !data && res.status(204).json(notFound);
+    res.status(201).render("formulario", {});
+});
 
 const server = app.listen(PORT, () => {
     console.log(
